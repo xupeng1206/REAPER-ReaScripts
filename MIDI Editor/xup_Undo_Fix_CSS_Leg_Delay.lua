138313198d484e0b791dd6f74da3aeca249ccfff
xupeng1206 <874582705@qq.com>
2021-04-15 21:32:34 +0800

    add undo actions
    Signed-off-by: xupeng1206 <874582705@qq.com>

---
+++ MIDI Editor/xup_Undo_Fix_CSS_Leg_Delay.lua
@@ -0,0 +1,71 @@
+--[[ 
+* ReaScript Name:Undo Fix CSS Leg Delay. 
+* Version: 2021/04/15 
+* Author: xupeng 
+* link: https://github.com/xupeng1206
+--]] 
+
+local r = reaper
+
+function MoveNote()
+    hwnd = r.MIDIEditor_GetActive()
+    take = r.MIDIEditor_GetTake(hwnd)
+
+    tick_1_14note = r.SNM_GetIntConfigVar("miditicksperbeat", -1)
+
+    retval, notecnt, _, _ = r.MIDI_CountEvts(take)
+    if notecnt == 0 then 
+        r.MB('No notes in the MIDI item.','ERROR',0) 
+        return
+    end
+
+    if notecnt > 0 then
+        for i = 0, notecnt-1 do
+            retval, sel, muted, startpos, endpos, chan, pitch, vel = r.MIDI_GetNote(take, i)
+            if sel then
+                -- set edit cursor at the start of the note
+                r.SetEditCurPos(r.MIDI_GetProjTimeFromPPQPos(take, startpos), true, false)
+                
+                tempo = r.Master_GetTempo()
+                ms_1_14note = 60 * 1000 / tempo
+                
+                tick1ms = tick_1_14note / ms_1_14note
+
+                posDiff = 0
+                if vel >= 0 and vel <= 64 then
+                    posDiff = 333 * tick1ms
+                elseif vel >= 65 and vel <= 100 then
+                    posDiff = 250 * tick1ms
+                elseif vel >=101 and vel <=127 then
+                    posDiff = 100 * tick1ms
+                else
+                    posDiff = 0
+                end
+
+                newStartPos = startpos + posDiff
+                if newStartPos < 0 then
+                    newStartPos = 0
+                end
+                
+                r.MIDI_SetNote(take, i, sel, muted, newStartPos, endpos, chan, pitch, vel) 
+            end
+        end
+    end
+end
+
+
+function main()
+    r.Undo_BeginBlock()
+    MoveNote()
+    r.Undo_EndBlock("Undo Fix CSS Leg Delay", 0)
+end
+
+r.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+main()
+
+r.TrackList_AdjustWindows(false)
+
+r.UpdateArrange() -- Update the arrangement (often needed)
+
+r.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
---
+++ MIDI Editor/xup_Undo_Fix_CSS_Short_Delay.lua
@@ -0,0 +1,62 @@
+--[[ 
+* ReaScript Name:Undo Fix CSS Short Delay. 
+* Version: 2021/04/15 
+* Author: xupeng 
+* link: https://github.com/xupeng1206
+--]] 
+
+local r = reaper
+
+function MoveNote()
+    hwnd = r.MIDIEditor_GetActive()
+    take = r.MIDIEditor_GetTake(hwnd)
+
+    tick_1_14note = r.SNM_GetIntConfigVar("miditicksperbeat", -1)
+
+    retval, notecnt, _, _ = r.MIDI_CountEvts(take)
+    if notecnt == 0 then 
+        r.MB('No notes in the MIDI item.','ERROR',0) 
+        return
+    end
+
+    if notecnt > 0 then
+        for i = 0, notecnt-1 do
+            retval, sel, muted, startpos, endpos, chan, pitch, vel = r.MIDI_GetNote(take, i)
+            if sel then
+                -- set edit cursor at the start of the note
+                r.SetEditCurPos(r.MIDI_GetProjTimeFromPPQPos(take, startpos), true, false)
+                
+                tempo = r.Master_GetTempo()
+                ms_1_14note = 60 * 1000 / tempo
+                
+                tick1ms = tick_1_14note / ms_1_14note
+
+                posDiff = 60 * tick1ms
+
+                newStartPos = startpos + posDiff
+                if newStartPos < 0 then
+                    newStartPos = 0
+                end
+                
+                r.MIDI_SetNote(take, i, sel, muted, newStartPos, endpos, chan, pitch, vel) 
+            end
+        end
+    end
+end
+
+
+function main()
+    r.Undo_BeginBlock()
+    MoveNote()
+    r.Undo_EndBlock("Undo Fix CSS Short Delay", 0)
+end
+
+r.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+main()
+
+r.TrackList_AdjustWindows(false)
+
+r.UpdateArrange() -- Update the arrangement (often needed)
+
+r.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
