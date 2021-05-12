41cdf2cc8585277bfbcbc6a730329e6cf5413bd2
xupeng1206 <xupeng@turingvideo.net>
2021-05-12 11:37:29 +0800

    update 4 actions
    Signed-off-by: xupeng1206 <xupeng@turingvideo.net>

---
+++ MIDI Editor/xup_Fix_CSS_Leg_Delay_By_Move.lua
@@ -0,0 +1,76 @@
+--[[ 
+* ReaScript Name:Fix CSS Leg Delay By Move. 
+* Version: 2021/05/12
+* Author: xupeng 
+* link: https://github.com/xupeng1206
+--]] 
+
+local r = reaper
+
+function MoveNote()
+    hwnd = r.MIDIEditor_GetActive()
+    take = r.MIDIEditor_GetTake(hwnd)
+
+    tick_1_14note = r.SNM_GetIntConfigVar("miditicksperbeat", -1)
+
+    retval, notecnt, _, _ = r.MIDI_CountEvts(take)
+    if notecnt == 0 then 
+        r.MB('No notes in the MIDI item.','ERROR',0) 
+        return
+    end
+
+    if notecnt > 0 then
+        for i = 0, notecnt-1 do
+            retval, sel, muted, startpos, endpos, chan, pitch, vel = r.MIDI_GetNote(take, i)
+            if sel then
+                -- set edit cursor at the start of the note
+                r.SetEditCurPos(r.MIDI_GetProjTimeFromPPQPos(take, startpos), true, false)
+                
+                tempo = r.Master_GetTempo()
+                ms_1_14note = 60 * 1000 / tempo
+                
+                tick1ms = tick_1_14note / ms_1_14note
+
+                posDiff = 0
+                if vel >= 0 and vel <= 64 then
+                    posDiff = -333 * tick1ms
+                elseif vel >= 65 and vel <= 100 then
+                    posDiff = -250 * tick1ms
+                elseif vel >=101 and vel <=127 then
+                    posDiff = -100 * tick1ms
+                else
+                    posDiff = 0
+                end
+
+                newStartPos = startpos + posDiff
+                if newStartPos < 0 then
+                    newStartPos = 0
+                end
+
+                newEndPos = endpos + posDiff
+                if newEndPos < 0 then
+                    newEndPos = 0
+                end
+                
+                r.MIDI_SetNote(take, i, sel, muted, newStartPos, newEndPos, chan, pitch, vel) 
+            end
+        end
+    end
+end
+
+
+function main()
+    r.Undo_BeginBlock()
+    MoveNote()
+    r.Undo_EndBlock("Fix CSS Leg Delay By Move", 0)
+end
+
+r.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+main()
+
+r.TrackList_AdjustWindows(false)
+
+r.UpdateArrange() -- Update the arrangement (often needed)
+
+r.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
